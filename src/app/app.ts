f272650d24c570f7ce3b2069c35c32bf394c6425
gogulpradeep <mgogulpradeep@gmail.com>
2025-11-17 14:56:36 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('myapp');
+  name= 'puli';
+  imageUrl = 'https://picsum.photos/200/300';
+  text = 'good evening'
 }
